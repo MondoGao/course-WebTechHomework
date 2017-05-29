033a8e2ebef8faedd5e547ec2e1894d8f79a16ca
Mondo <mondogao@gmail.com>
2017-05-29 23:39:32 +0800

    feat: 增加前端基本框架

---
+++ Website/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './App.css';
+
+import {
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+const App = (props) => (
+  <div>
+    hello world!
+  </div>
+)
+
+export default App;
---
+++ Website/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import App from 'components/App';
+
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+        {Component}
+    </AppContainer>,
+    document.getElementById('app')
+  );
+};
+
+render(
+  <Router>
+    <Route path="/" component={App}/>
+  </Router>
+);
+
+if (module.hot) {
+  module.hot.accept('components/App', () => {
+    render(
+      <Router>
+        <Route path="/" component={App}/>
+      </Router>
+    )
+  });
+}
